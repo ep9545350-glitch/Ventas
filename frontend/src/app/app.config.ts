ad071724dbbbf8d916fd20774ab5990d8854bdfa
ep9545350-glitch <ep9545350@gmail.com>
2026-07-21 09:54:54 -0500

    final actualziado

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,21 +1,32 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection
+} from '@angular/core';
+
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 
 import { routes } from './app.routes';
-import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-
 import { authInterceptor } from './services/auth.interceptor';
 
-export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes),
 
-    provideClientHydration(withEventReplay()),
+export const appConfig: ApplicationConfig = {
+
+  providers: [
+
+    provideBrowserGlobalErrorListeners(),
+
+    provideZoneChangeDetection({
+      eventCoalescing: true
+    }),
+
+    provideRouter(routes),
 
     provideHttpClient(
       withInterceptors([authInterceptor])
     )
+
   ]
+
 };
